38b544cafad11872d03926db3a1390fc32c6f295
FreecoMwaniki <freecomwaniki@gmail.com>
2024-12-06 23:21:25 +0300

    updated user

--- springSecurityAdmin-User-Login/src/main/java/com/example/springSecurityAdmin/User/Login/GreetingsController.java
+++ springSecurityAdmin-User-Login/src/main/java/com/example/springSecurityAdmin/User/Login/GreetingsController.java
@@ -1,18 +1,32 @@
 package com.example.springSecurityAdmin.User.Login;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/employees")
 public class GreetingsController {
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Bonjour";
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('ROLE_USER'or hasRole(ROLE_ADMIN))")
+    public String getAllUsers(){
+        return "THis are all your Employees";
     }
-    @PostMapping
-    public String saveEmployee(){
-        return "You have saved a new employee";
+
+    @PostMapping("/api")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String saveUsers(){
+        return "You have added a user";
     }
-    @PutMapping
-    public String updateEmployee
+
+    @PutMapping("/user/{ID}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String updateUser(){
+        return "you have just updated a user";
+    }
+
+    @DeleteMapping("/user/{ID}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String deleteUSer(){
+        return "you have just deleted a user";
+    }
 }
